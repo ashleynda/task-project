d15ade504ff33f797f706a7aee8dbdc18476d4e8
k33ngTomide <Msodiqakintomide@gmail.com>
2023-11-29 16:17:08 +0100

    Added the route Number for all category options

--- dashboard.js
+++ dashboard.js
@@ -23,6 +23,85 @@ startButton.addEventListener('click', () => {
 
 })
 
+let routeNumber = 0;
+
+switch (category){
+  case "General Knowledge":
+    routeNumber = 9;
+    break;
+  case "Entertainment: Books":
+    routeNumber = 10;
+    break;
+  case " Entertainment: Film":
+    routeNumber = 11;
+    break;
+  case " Entertainment: Music":
+    routeNumber = 12;
+    break;
+  case " Entertainment: Musicals & Theatres":
+    routeNumber = 13;
+    break;
+  case " Entertainment: Television":
+    routeNumber = 14;
+    break;
+  case " Entertainment: Video Games":
+    routeNumber = 15;
+    break;
+  case " Entertainment: Board Games":
+    routeNumber = 16;
+    break;
+  case " Science & Natures":
+    routeNumber = 17;
+    break;
+  case " Science: Computers":
+    routeNumber = 18;
+    break;
+  case " Science: Mathematics":
+    routeNumber = 19;
+    break;
+  case " Mythology":
+    routeNumber = 20;
+    break;
+  case " Sports":
+    routeNumber = 21;
+    break;
+  case " Geography":
+    routeNumber = 22;
+    break;
+  case " History":
+    routeNumber = 23;
+    break;
+  case " Politics":
+    routeNumber = 24;
+    break;
+  case " Arts":
+    routeNumber = 25;
+    break;
+  case " Celebrities":
+  routeNumber = 26;
+  break;
+  case " Animals":
+    routeNumber = 27;
+    break;
+  case " Vehicles":
+    routeNumber = 28;
+    break;
+  case " Entertainment: Comics":
+    routeNumber = 29;
+    break;
+  case " Science: Gadgets":
+    routeNumber = 30;
+    break;
+  case " Entertainment: Japanese Anime & Manga":
+    routeNumber = 31;
+    break;
+  case " Entertainment: Cartoons & Animation":
+    routeNumber = 32;
+    break;
+  
+}
+
+
 
 function getQuestions(){
 
@@ -38,6 +117,5 @@ function getQuestions(){
     console.log(allQuestions.questions);
     
   }
-
 
 }
