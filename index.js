5155b8dae5415b1602fa077683ff276a57ef98ea
k33ngTomide <Msodiqakintomide@gmail.com>
2023-11-30 08:18:06 +0100

    Completed task

--- index.js
+++ index.js
@@ -1,22 +1,21 @@
 //write your code here
 
-let firstName = document.getElementById("firstname");
-let lastName = document.getElementById("lastname");
-let phoneNumber = document.getElementById("phone-number");
-let email = document.getElementById("email");
-let password = document.getElementById("password");
-
-let fullName = firstName.value + lastName.value;
-console.log(fullName);
-let userFullName = localStorage.setItem("userFullName", fullName);
-let userPhoneNumber = sessionStorage.setItem("userPhoneNumber", phoneNumber);
-let userEmail = sessionStorage.setItem("userEmail", email);
-
-
-
 let form = document.getElementById("form");
 form.addEventListener('submit', (event) => {
   event.preventDefault();
+
+  let firstName = document.getElementById("firstname").value;
+  let lastName = document.getElementById("lastname").value;
+  let phoneNumber = document.getElementById("phone-number").value;
+  let email = document.getElementById("email").value;
+  let password = document.getElementById("password").value;
+
+  let fullName = firstName + " " + lastName;
+  console.log(fullName);
+  let userFullName = localStorage.setItem("userFullName", fullName);
+  let userPhoneNumber = localStorage.setItem("userPhoneNumber", phoneNumber);
+  let userEmail = localStorage.setItem("userEmail", email);
+  
   window.location = './dashboard.html'
 })
 
